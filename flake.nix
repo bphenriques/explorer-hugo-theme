230f3f89b1c7673b7b4a3d40c64f3c0d95aabe48
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2022-05-31 10:22:12 +0100

    dev: direnv with Nix Flake

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "explorer-hugo-theme";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShell = import ./shell.nix { inherit pkgs; };
+        }
+      );
+}
